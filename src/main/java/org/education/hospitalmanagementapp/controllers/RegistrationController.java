ba3e4266b1a0495ba946a8ccf39fefa901e5a7cc
philippejean <phil09492@gmail.com>
2024-10-28 23:41:38 -0400

    Pushing the two fxmls( RegistrationView.fxml and LoginView.fxml) and the controllers

---
+++ src/main/java/org/education/hospitalmanagementapp/controllers/RegistrationController.java
@@ -0,0 +1,53 @@
+package org.education.hospitalmanagementapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class RegistrationController {
+
+    @FXML
+    private TextField FirstNameField;
+
+    @FXML
+    private ImageView calendar_image;
+
+    @FXML
+    private TextField confirmPassField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private Button login_Button;
+
+    @FXML
+    private ImageView menu;
+
+    @FXML
+    private ImageView noti_image;
+
+    @FXML
+    private Label num_of_noti;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private ImageView tosButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void loginUser(ActionEvent event) {
+
+    }
+
+}
