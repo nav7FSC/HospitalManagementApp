9dcd57e11351e945761c11bf350792c378b74cd9
ahsan136 <mahsan2691@gmail.com>
2024-10-29 23:49:38 -0400

    AuthService: Database server connection added Added dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.education.hospitalmanagementapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.education.hospitalmanagementapp to javafx.fxml;
