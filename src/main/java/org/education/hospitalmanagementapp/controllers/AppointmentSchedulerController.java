9fb2db4bbc245382ab47505866418fb36412f61b
philippejean <phil09492@gmail.com>
2024-11-18 13:10:10 -0500

    Created AppointmentSchedulerController and ScheduleAppointment.fxml

---
+++ src/main/java/org/education/hospitalmanagementapp/controllers/AppointmentSchedulerController.java
@@ -0,0 +1,76 @@
+package org.education.hospitalmanagementapp.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
+
+public class AppointmentSchedulerController {
+
+    @FXML
+    private ToggleButton amtoggle;
+
+    @FXML
+    private Button confirmAppoitmeentButtn;
+
+    @FXML
+    private Label dateCancel;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Label date_Ok;
+
+    @FXML
+    private TextField doctorFirstName;
+
+    @FXML
+    private TextField doctorLastName;
+
+    @FXML
+    private TextField doctor_ID;
+
+    @FXML
+    private TextField hourField;
+
+    @FXML
+    private Button main_menu_bttn;
+
+    @FXML
+    private ImageView menu;
+
+    @FXML
+    private TextField minuteField;
+
+    @FXML
+    private ImageView noti_image;
+
+    @FXML
+    private Label num_of_noti;
+
+    @FXML
+    private TextField patientFirstName;
+
+    @FXML
+    private TextField patientLastName;
+
+    @FXML
+    private ToggleButton pmtoggle;
+
+    @FXML
+    private ImageView profile_Image;
+
+    @FXML
+    private Button signOut_Bttn;
+
+    @FXML
+    private Label time_Cancel;
+
+    @FXML
+    private Label time_Ok;
+
+}
