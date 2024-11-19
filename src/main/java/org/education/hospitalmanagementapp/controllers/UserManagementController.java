711bc2f74d3b5936ead0672e60f5e40bc85fefed
philippejean <phil09492@gmail.com>
2024-11-19 12:54:38 -0500

    Made User ManagementFxml and the controller

---
+++ src/main/java/org/education/hospitalmanagementapp/controllers/UserManagementController.java
@@ -0,0 +1,62 @@
+package org.education.hospitalmanagementapp.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class UserManagementController  {
+
+    @FXML
+    private ImageView clearCurrPassField;
+
+    @FXML
+    private ImageView clearNewPassField;
+
+    @FXML
+    private ImageView clearNewUserField;
+
+    @FXML
+    private ImageView clear_editUserNameField;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private TextField currPassField;
+
+    @FXML
+    private TextField edit_UserNameField;
+
+    @FXML
+    private ImageView edit_clearEmailfield;
+
+    @FXML
+    private TextField edit_emailField;
+
+    @FXML
+    private Button mainMenuBttn;
+
+    @FXML
+    private ImageView menu;
+
+    @FXML
+    private TextField newPassField;
+
+    @FXML
+    private TextField newUserField;
+
+    @FXML
+    private ImageView noti_image;
+
+    @FXML
+    private Label num_of_noti;
+
+    @FXML
+    private ImageView profile_Image;
+
+    @FXML
+    private Button signOutBttn;
+
+}
