7af0fbfbccc039f9ac034a848fd65c461cdc5ca3
Ashraf Sangi <ashraf.sangi27@gmail.com>
2024-11-25 11:23:26 -0500

    Created HospitalDashBoard.fxml and DashBoardController.java

--- src/main/java/org/education/hospitalmanagementapp/controllers/DashBoardController.java
+++ src/main/java/org/education/hospitalmanagementapp/controllers/DashBoardController.java
@@ -1,7 +1,13 @@
 package org.education.hospitalmanagementapp.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class DashBoardController {
 
@@ -10,5 +16,19 @@ public class DashBoardController {
 
     @FXML
     private Button dashSignOut;
+
+    @FXML
+    void goToMain(ActionEvent event){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/org.education.hospitalmanagementapp/MainMenu.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/org/education/hospitalmanagementapp/controllers/MainMenuController.java
+++ src/main/java/org/education/hospitalmanagementapp/controllers/MainMenuController.java
@@ -2,9 +2,14 @@ package org.education.hospitalmanagementapp.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class MainMenuController {
 
@@ -46,7 +51,16 @@ public class MainMenuController {
 
     @FXML
     void GoToUsrManagement(ActionEvent event) {
-
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/org.education.hospitalmanagementapp/HospitalDashBoard.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
