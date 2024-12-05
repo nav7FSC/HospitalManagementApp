495018aa7350201a40c0d9c1709b96e00e0a5df6
Philippe-Je <phil09492@gmail.com>
2024-12-05 18:25:41 -0500

    Made the Billing_And_Invoices.fxml and the Skeleton Controller

---
+++ src/main/java/org/education/hospitalmanagementapp/controllers/BillingAndInvoiceController.java
@@ -0,0 +1,72 @@
+package org.education.hospitalmanagementapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class BillingAndInvoiceController {
+
+    @FXML
+    private TextField billing_Id_Field;
+
+    @FXML
+    private Button chargeButton;
+
+    @FXML
+    private ImageView clearPriceField;
+
+    @FXML
+    private ImageView clearTypeField;
+
+    @FXML
+    private ImageView clear_Id;
+
+    @FXML
+    private ImageView clear_Invoice;
+
+    @FXML
+    private TextField invoice_Field;
+
+    @FXML
+    private Button mainmenuBttn;
+
+    @FXML
+    private ImageView menu;
+
+    @FXML
+    private ImageView noti_image;
+
+    @FXML
+    private Label num_of_noti;
+
+    @FXML
+    private TextField priceField;
+
+    @FXML
+    private ImageView profile_Image;
+
+    @FXML
+    private Button signOut_Bttn;
+
+    @FXML
+    private TextField typeOfServiceField;
+
+    @FXML
+    void calculatePatientCharge(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotoMain(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signOut(ActionEvent event) {
+
+    }
+
+}
