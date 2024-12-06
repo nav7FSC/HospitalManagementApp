13b7faf7877fab2f1c4ddc234ed78f35e08c1b3f
philippejean <phil09492@gmail.com>
2024-12-06 12:24:22 -0500

    Improved the Hospital_Inventory.fxml and made the Controller

---
+++ src/main/java/org/education/hospitalmanagementapp/controllers/InventoryController.java
@@ -0,0 +1,100 @@
+package org.education.hospitalmanagementapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class InventoryController{
+
+
+@FXML
+    private Button mainBttn;
+
+    @FXML
+    private ImageView menu;
+
+    @FXML
+    private ImageView noti_image;
+
+    @FXML
+    private Label numOfAnalgesics;
+
+    @FXML
+    private Label numOfAnesthesia;
+
+    @FXML
+    private Label numOfAntiBio;
+
+    @FXML
+    private Label numOfAntiDep;
+
+    @FXML
+    private Label numOfAntiemetics;
+
+    @FXML
+    private Label numOfBeds;
+
+    @FXML
+    private Label numOfDE;
+
+    @FXML
+    private Label numOfEkg;
+
+    @FXML
+    private Label numOfGD;
+
+    @FXML
+    private Label numOfGloves;
+
+    @FXML
+    private Label numOfGowns;
+
+    @FXML
+    private Label numOfHepaB;
+
+    @FXML
+    private Label numOfHpv;
+
+    @FXML
+    private Label numOfInfluenza;
+
+    @FXML
+    private Label numOfLD;
+
+    @FXML
+    private Label numOfPneumo;
+
+    @FXML
+    private Label numOfSC;
+
+    @FXML
+    private Label numOfSE;
+
+    @FXML
+    private Label numOfShingles;
+
+    @FXML
+    private Label numOfVentilators;
+
+    @FXML
+    private Label num_of_noti;
+
+    @FXML
+    private ImageView profile_Image;
+
+    @FXML
+    private Button signOutBttn;
+
+    @FXML
+    void goToMain(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signOut(ActionEvent event) {
+
+    }
+
+}
