7231f29bdca4525c89a0ec970adf67f5dd83bf30
Philippe-Je <phil09492@gmail.com>
2024-12-05 19:44:54 -0500

    Made the HospitalPatientManagement.fxml and the skeleton cotroller PatientManagerController

---
+++ src/main/java/org/education/hospitalmanagementapp/controllers/PatientManagerController.java
@@ -0,0 +1,92 @@
+package org.education.hospitalmanagementapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class PatientManagerController {
+
+    @FXML
+    private TextField adressField;
+
+    @FXML
+    private Button changePatientBttn;
+
+    @FXML
+    private ImageView claerID;
+
+    @FXML
+    private ImageView clearAddressField;
+
+    @FXML
+    private ImageView clearEmailField;
+
+    @FXML
+    private ImageView clearFirstNameField;
+
+    @FXML
+    private ImageView clearLastNameField;
+
+    @FXML
+    private ImageView clearPhoneNum;
+
+    @FXML
+    private Button confirmPatienBttn;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private Button mainMenuBttn;
+
+    @FXML
+    private ImageView menu;
+
+    @FXML
+    private ImageView noti_image;
+
+    @FXML
+    private Label num_of_noti;
+
+    @FXML
+    private TextField patienIDField;
+
+    @FXML
+    private TextField phoneNumField;
+
+    @FXML
+    private ImageView profile_Image;
+
+    @FXML
+    private Button signOutBttn;
+
+    @FXML
+    void changePatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotoMain(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signOutAction(ActionEvent event) {
+
+    }
+
+}
