68185c9a25c363ab5567f9a74e79ae6b3d1accba
nav7FSC <singn7@farmingdale.edu>
2024-12-08 20:29:20 -0500

    Replaced TextField with PasswordField in both the login and registration FXML to ensure passwords are masked during input. This change enhances security and prevents sensitive data from being visible as users type.

--- src/main/java/org/education/hospitalmanagementapp/controllers/LoginViewController.java
+++ src/main/java/org/education/hospitalmanagementapp/controllers/LoginViewController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.education.hospitalmanagementapp.AlertMessages;
@@ -17,10 +18,10 @@ public class LoginViewController {
     private static AuthServiceClass asc = new AuthServiceClass();
     public static String username;
 
-
-
     @FXML
-    private TextField userField, passField, emailField;
+    private TextField userField, emailField;
+    @FXML
+    private PasswordField passField;
 
     public String getuser()
     {
